b1f84eea0bac145d5bceb276feaf973b38a5b38e
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-08-30 11:05:49 -0400

    added trees(binary and n-ary tree

--- linkedList.js
+++ linkedList.js
@@ -170,4 +170,312 @@ class LinkedList {
   }
 }
 
-module.exports = LinkedList;
+// ***************************************    Binary Tree   ****************************
+
+
+/** BinaryTreeNode: node for a general tree. */
+
+class BinaryTreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** minDepth(): return the minimum depth of the tree -- that is,
+   * the length of the shortest path from the root to a leaf. */
+
+  minDepth() {
+    if (!this.root) return 0;
+
+    function minDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return minDepthHelper(node.right) + 1;
+      if (node.right === null) return minDepthHelper(node.left) + 1;
+      return (
+        Math.min(minDepthHelper(node.left), minDepthHelper(node.right)) + 1
+      );
+    }
+
+    return minDepthHelper(this.root);
+  }
+
+  /** maxDepth(): return the maximum depth of the tree -- that is,
+   * the length of the longest path from the root to a leaf. */
+
+  maxDepth() {
+    if (!this.root) return 0;
+
+    function maxDepthHelper(node) {
+      if (node.left === null && node.right === null) return 1;
+      if (node.left === null) return maxDepthHelper(node.right) + 1;
+      if (node.right === null) return maxDepthHelper(node.left) + 1;
+      return (
+        Math.max(maxDepthHelper(node.left), maxDepthHelper(node.right)) + 1
+      );
+    }
+
+    return maxDepthHelper(this.root);
+  }
+
+  /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
+   * The path doesn't need to start at the root, but you can't visit a node more than once. */
+
+  maxSum() {
+    let result = 0;
+
+    function maxSumHelper(node) {
+      if (node === null) return 0;
+      const leftSum = maxSumHelper(node.left);
+      const rightSum = maxSumHelper(node.right);
+      result = Math.max(result, node.val + leftSum + rightSum);
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+
+    maxSumHelper(this.root);
+    return result;
+  }
+
+  /** nextLarger(lowerBound): return the smallest value in the tree
+   * which is larger than lowerBound. Return null if no such value exists. */
+
+  nextLarger(lowerBound) {
+    if (!this.root) return null;
+
+    // let's use BFS for this!
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    return closest;
+  }
+
+  /** Further study!
+   * areCousins(node1, node2): determine whether two nodes are cousins
+   * (i.e. are at the same level but have different parents. ) */
+
+  areCousins(node1, node2) {
+    if (node1 === this.root || node2 === this.root) return false;
+
+    function findLevelAndParent(
+      nodeToFind,
+      currentNode,
+      level = 0,
+      data = { level: 0, parent: null }
+    ) {
+      if (data.parent) return data;
+      if (currentNode.left === nodeToFind || currentNode.right === nodeToFind) {
+        data.level = level + 1;
+        data.parent = currentNode;
+      }
+      if (currentNode.left) {
+        findLevelAndParent(nodeToFind, currentNode.left, level + 1, data);
+      }
+      if (currentNode.right) {
+        findLevelAndParent(nodeToFind, currentNode.right, level + 1, data);
+      }
+      return data;
+    }
+
+    let node1Info = findLevelAndParent(node1, this.root);
+    let node2Info = findLevelAndParent(node2, this.root);
+
+    let sameLevel =
+      node1Info && node2Info && node1Info.level === node2Info.level;
+    let differentParents =
+      node1Info && node2Info && node1Info.parent !== node2Info.parent;
+    return sameLevel && differentParents;
+  }
+
+  /** Further study!
+   * serialize(tree): serialize the BinaryTree object tree into a string.
+   *
+   * Output looks like this:
+   *   1 2 # # 3 4 # # 5 # #
+   *  Where # represents # children with a pre-order traversal.
+   */
+
+  static serialize(tree) {
+    const values = [];
+
+    function traverse(node) {
+      if (node) {
+        values.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+      } else {
+        values.push("#");
+      }
+    }
+
+    traverse(tree.root);
+    return values.join(" ");
+  }
+
+  /** Further study!
+   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
+
+  static deserialize(stringTree) {
+    if (!stringTree) return null;
+
+    const values = stringTree.split(" ");
+
+    function buildTree() {
+      // building a tree starting from the beginning of the array
+      if (values.length) {
+        const currentVal = values.shift();
+
+        if (currentVal === "#") return null;
+
+        // remember to convert values back into numbers
+        let currentNode = new BinaryTreeNode(+currentVal);
+        currentNode.left = buildTree();
+        currentNode.right = buildTree();
+
+        return currentNode;
+      }
+    }
+
+    const root = buildTree();
+    return new BinaryTree(root);
+  }
+
+  /** Further study!
+   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
+   * of two nodes in a binary tree. */
+
+  lowestCommonAncestor(node1, node2, currentNode=this.root) {
+    // base case 1: empty tree
+    if (currentNode === null) return null;
+
+    // base case 2: root is one of the target nodes
+    if (currentNode === node1 || currentNode === node2) return currentNode;
+
+    // recursively search the left sub-tree
+    const left = this.lowestCommonAncestor(node1, node2, currentNode.left);
+
+    // recursively search the right sub-tree
+    const right = this.lowestCommonAncestor(node1, node2, currentNode.right);
+
+    // if neither left nor right is null, currentNode is the ancestor
+    if (left !== null && right !== null) return currentNode;
+    
+    // if one node is not null, return it
+    if (left !== null || right !== null) return left || right;
+    
+    // left and right are both null, return null
+    if (left === null && right === null) return null;
+  }
+}
+
+
+
+//  ************************************    General Tree    *****************************************
+
+
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+
+  sumValues() {
+    if (!this.root) return 0;
+
+    let total = this.root.val;
+
+    function sumHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // accumulate all values
+        total += child.val;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          sumHelper(child);
+        }
+      }
+    }
+
+    sumHelper(this.root);
+    return total;
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+
+  countEvens() {
+    if (!this.root) return 0;
+
+    let count = this.root.val % 2 === 0 ? 1 : 0;
+
+    function countEvensHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // count the child if the value is even
+        if (child.val % 2 === 0) count++;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          countEvensHelper(child);
+        }
+      }
+    }
+
+    countEvensHelper(this.root);
+    return count;
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+
+  numGreater(lowerBound) {
+    if (!this.root) return 0;
+
+    let count = this.root.val > lowerBound ? 1 : 0;
+
+    function countEvensHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // count the child if the value is greater than lowerBound
+        if (child.val > lowerBound) count++;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          countEvensHelper(child);
+        }
+      }
+    }
+
+    countEvensHelper(this.root);
+    return count; 
+  }
+}
